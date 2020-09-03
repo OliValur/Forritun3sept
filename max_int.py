6d8d359b267b7188f633552fd3c3fb43671c14cd
Olafur Valur Sigurdsson <olivalur85@gmail.com>
2020-09-03 13:23:45 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,15 @@
+#taka user input
+# maxnum=0
+# while lykkja meðan input ekki <0
+#     ef input > maxnum   
+#         maxnum = input
+#     nýtt input
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+
+while num_int >= 0:
+    if num_int > max_int:
+
+
+print("The maximum is", max_int)    # Do not change this line
